bb6f5a91470d9551e59a888e9fea3c647bcfecfc
Ian Marshall <ian.js.tutor@gmail.com>
2022-08-23 13:50:28 -0400

    working code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,82 @@
 //DOM elements
-const main = document.querySelector("main");
-const slider = document.querySelector("nav input");
-const [html, static, animation] = document.querySelectorAll("footer textarea");
-const [staticStyle, animationStyle] = document.head.querySelectorAll("style");
+const div = document.querySelector("div");
+const playButton = document.querySelector("button#playpause");
+const input = document.querySelector("input");
 
 //state variables
-var id = "myDiv";
+var interval;
 
-//page load setup
-html.value = main.innerHTML.trim();
-changeStatic();
-changeAnimation();
-
-//state event listeners
-html.addEventListener("change", changeHTML);
-static.addEventListener("change", changeStatic);
-animation.addEventListener("change", changeAnimation);
-
-//state change
-function changeHTML() {
-    main.innerHTML = html.value.trim();
+//onload setup
+init: {
+	input.addEventListener("input", scrub);
+    playButton.addEventListener("click", play);
+	document.querySelector("button#rewind").addEventListener("click", rewind);
+	document.querySelector("fieldset button").addEventListener("click", changeSettings);
+    changeSettings();
 }
 
-function changeStatic() {
-    staticStyle.innerHTML = `${ static.value.trim() } #${id}{animation-play-state:paused}#${id}.playing{animation-play-state:running;}`;
+//controllers
+function scrub(){
+	clearAnimation();
+	setAnimationDelay();
+	stopAnimation();
+	playButton.innerHTML = "&#9658;";
 }
 
-function changeAnimation() {
-    animationStyle.innerHTML = animation.value.trim();
+function play(){
+	if (div.classList.contains("playing")){
+		playButton.innerHTML = "&#9658;";
+		div.classList.remove("playing");
+		stopAnimation();
+	}
+	else {
+		clearAnimation();
+		setAnimationDelay();
+		startAnimation();
+		playButton.innerHTML = "<span>&#10074;&#10074;</span>";
+		div.classList.add("playing");
+	}
+}
+
+function setAnimationDelay(){
+	div.style.animationDelay = -input.value + "s";
+}
+
+function clearAnimation(){
+	div.classList.remove("playing");
+	div.style.animation = "none";
+	div.offsetHeight;
+	div.style.animation = null;
+}
+
+function startAnimation(){
+	interval = setInterval(tick, 100);
+}
+
+function tick(){
+	input.value = Number(input.value) + 0.1;
+}
+
+function stopAnimation(){
+	clearInterval(interval);
+}
+
+function rewind(){
+	input.value = 0;
+	scrub();
+}
+
+function changeSettings(){
+	let static = document.querySelector("#static").value,
+		from = document.querySelector("#from").value,
+		to = document.querySelector("#to").value,
+		styleElement = document.querySelector("#testdiv1style");
+	styleElement.innerHTML = `
+		#testdiv1 {${static} animation: testdiv1anim 5s forwards; animation-play-state: paused; }
+		#testdiv1.playing { animation-play-state: running; }
+		@keyframes testdiv1anim {
+			  0% {${from}}
+			100% {${to}}
+		}
+	`;
 }
